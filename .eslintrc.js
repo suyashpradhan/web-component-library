c93428b5f3308997ca22f2da85df2638eb9ee1a4
suyash-bureau <pradhangroup@hotmail.com>
2022-07-15 18:34:26 +0530

    feat: adds storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 /** @format */
-
 module.exports = {
   env: {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {

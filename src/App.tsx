6f39103b0bf1f2043810b746cd43c0d120ac4504
suyash-bureau <pradhansuyash39@gmail.com>
2022-07-22 23:43:09 +0530

    feat: adds button stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 
-export default function App(): ReactElement {
+export default function App(): React.ReactElement {
   return <h1>hello world</h1>;
 }
